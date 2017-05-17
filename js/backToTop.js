01377ce1b7ca4b9e39595beb74cbc54fde46f5d9
Peter Januarius <pete@nexgencodecamp.com.au>
2017-05-17 16:42:48 +1000

    Added favicon, rearranged CSS and Script links, added a logo, fixed typos, changed links to buttons.

--- js/backToTop.js
+++ js/backToTop.js
@@ -1,20 +1,24 @@
-$(document).ready(function()
-{
+$(document).ready(function() {
   var btt = $(".back-to-top");
-  btt.on("click", function(e)
-{
-  $("html, body").animate(
-    {
-      scrollTop: 0
-    }, 500);
+  btt.on("click", function(e) {
+      $("html, body").animate({
+          scrollTop: 0
+      }, 500);
+      e.preventDefault();
+  });
 
-    e.preventDefault();
-});
-  $(window).on("scroll", function()
-  {
+  $('#btnWebsite').on('click', function(){
+      location.href="https://nexgencodecamp.com.au";
+  })
+  $('#btnBlog').on('click', function(){
+      location.href="http://blog.nexgencodecamp.com.au";
+  })
+
+  $(window).on("scroll", function() {
     var self = $(this),
     height = self.height(),
     top = self.scrollTop();
-
   });
+
+
 });
